342cf9d932eac72ffbd2040bd87ab458f3bbc3eb
Vansh Itendra Rugnate <vansh.rugnate123@outlook.com>
2025-05-27 18:56:10 +0100

    Create nav bar

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-git
+from flask import Flask, render_template, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("homepage.html", pagename="Home")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
