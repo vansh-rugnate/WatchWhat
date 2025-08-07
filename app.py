5429b5e7e36776384ef4e70b792fa972a8bdcda0
Vansh Itendra Rugnate <vansh.rugnate123@outlook.com>
2025-08-07 22:14:38 +0100

    Added a form where user can apply tvshow filters and where clicking submit button will show names of all tvshows fitting the criteria

--- app.py
+++ app.py
@@ -142,8 +142,11 @@ def search_movies():
                 for movie in movies_data:
                     movies.append(movie['original_title'] + ":" + str(movie['id']))
             # Randomly choose one of those movies
-            random_num = randint(0,len(movies)-1)
-            random_movie = movies[random_num]
+            if len(movies) > 0:
+                random_num = randint(0,len(movies)-1)
+                random_movie = movies[random_num]
+            else:
+                return render_template("tvshows.html", error="Could not find any matches. Please adjust the filters.")
             # Get a poster for the movie
             movie_title = random_movie.split(":")[0]
             movie_id = random_movie.split(":")[1]
@@ -162,12 +165,66 @@ def search_movies():
         return redirect(url_for("login"))
 
 
-@app.route("/tvshows")
+@app.route("/tvshows", methods=["GET", "POST"])
 def search_tvshows():
     if "username" in session:
+        raw_api_data = None
+        tvshows = []
+        if request.method == "POST":
+            # Handle first page of API Data
+            url = "https://api.themoviedb.org/3/discover/tv"
+            headers = {
+                "accept": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MWVhNjI5MWJjYjA0MjVlYTRiZWJmMTNmOTcyOTVlMiIsIm5iZiI6MTc1MzI5NDQzOS44MjA5OTk5LCJzdWIiOiI2ODgxMjY2NzZhMzgwNGMyMGUxNmFmZDAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5xA_CIxcW_VCkJPl-NrOyEUobnBwndzWmrhijGVc62o"
+            }
+            params = {
+                "with_genres" : request.form["genre"],
+                "with_runtime.lte" : request.form["runtime_lte"],
+                "with_original_language" : request.form["original_language"],
+                "first_air_date_year" : request.form["first_air_date_year"]
+            }
+            response = requests.get(url, headers=headers, params=params)
+            raw_api_data = response.json()
+            total_pages = raw_api_data['total_pages'] # Get total number of pages from API response
+            tvshows_data = raw_api_data['results']
+            for tvshow in tvshows_data:
+                tvshows.append(tvshow['original_name'] + ":" + str(tvshow['id']))
+            # Handle rest of the pages of API Data
+            for i in range(2,total_pages+1):
+                params = {
+                    "with_genres" : request.form["genre"],
+                    "with_runtime.lte" : request.form["runtime_lte"],
+                    "with_original_language" : request.form["original_language"],
+                    "first_air_date_year" : request.form["first_air_date_year"],
+                    "page" : i
+                }
+                response = requests.get(url, headers=headers, params=params)
+                raw_api_data = response.json()
+                tvshows_data = raw_api_data['results']
+                for tvshow in tvshows_data:
+                    tvshows.append(tvshow['original_name'] + ":" + str(tvshow['id']))
+            # Randomly choose one of those movies
+            if len(tvshows) > 0:
+                random_num = randint(0,len(tvshows)-1)
+                random_tvshow = tvshows[random_num]
+            else:
+                return render_template("tvshows.html", error="Could not find any matches. Please adjust the filters.")
+            # Get a poster for the movie
+            tvshow_name = random_tvshow.split(":")[0]
+            tvshow_id = random_tvshow.split(":")[1]
+            url = f"https://api.themoviedb.org/3/tv/{tvshow_id}/images"
+            response = requests.get(url, headers=headers)
+            raw_api_data_images = response.json()
+            poster = raw_api_data_images.get('posters', [])
+            if poster:
+                poster_url = poster[0]['file_path']
+            else:
+                poster_url = None
+            
+            return render_template("tvshows.html", title=tvshow_name, poster_url_path=poster_url)
         return render_template("tvshows.html")
     else:
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
     
 
 @app.route("/all-tv")
@@ -175,7 +232,7 @@ def search_all():
     if "username" in session:
         return render_template("all-tv.html")
     else:
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
     
 
 @app.route("/aboutme")
